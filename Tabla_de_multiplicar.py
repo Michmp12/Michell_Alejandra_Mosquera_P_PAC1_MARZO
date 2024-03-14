cba60815946e7d04b3736ac0a95d89dec2a7bf78
MichMP <158522214+Michmp12@users.noreply.github.com>
2024-03-14 16:18:48 -0500

    Update Tabla_de_multiplicar.py

--- Tabla_de_multiplicar.py
+++ Tabla_de_multiplicar.py
@@ -1,7 +1,7 @@
 numeroInt = int(input('Ingrese por favor un número entero positivo:'))
 
 if numeroInt <= 0:
-    print("El número ingresado debe ser positivo.Intenta nuevamente")
+    print("El número ingresado debe ser positivo, Por favor intenta nuevamente")
 else: 
     print('Tabla de multiplicar del: ',numeroInt)
     for i in range(1, 11):
